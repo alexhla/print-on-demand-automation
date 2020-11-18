8f1e21c05d2bddd6c62c2259971ca9e75ca7bf16
alexhla <astutealex@gmail.com>
2020-11-18 15:19:37 -0500

    added GET product info method calling printful REST api

---
+++ printful.py
@@ -0,0 +1,28 @@
+from config import API_KEY
+import requests
+import json
+
+URL_BASE = 'https://api.printful.com'
+
+def get_product_types():
+	product_types = {}
+	response = requests.get(URL_BASE + '/products')
+	for product in response.json()['result']:
+		if product["type"] in product_types:
+			product_types[product["type"]] += 1
+		else:
+			product_types[product["type"]] = 1
+	return(product_types)
+
+def print_product_info(product_type):
+	product_info = []
+	response = requests.get(URL_BASE + '/products')
+	for product in response.json()['result']:	
+		if product["type"] == product_type:
+			product_info.append(product)
+	return(product_info)
+
+print(f'Product Types: {get_product_types()}')
+
+for item in print_product_info("MUG"):
+	print(f'\nProduct Info: {item}')
