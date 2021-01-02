cffb95aefaa1cfd4d9b3330bcfc24c03e55aabd5
alexhla <astutealex@gmail.com>
2021-01-02 00:47:40 -0500

    added canvas and poster product lookup in printify_read_catalog.py

--- printify_read_catalog.py
+++ printify_read_catalog.py
@@ -6,27 +6,44 @@ import os
 import config
 
 HEADER = {'Authorization': 'Bearer {}'.format(config.PRINTIFY_API_TOKEN)}
-URL_BASE = 'https://api.printify.com/v1/catalog/'
-URL_BLUEPRINTS = 'blueprints.json'
-URL_PRINT_PROVIDERS = 'print_providers.json'
+URL_BASE = 'https://api.printify.com'
+URL_BLUEPRINTS = '/v1/catalog/blueprints.json'
+URL_PRINT_PROVIDERS = '/v1/catalog/print_providers.json'
 
-
-#### GET list of printify providers
-# endpoint = os.path.join(URL_BASE, URL_PRINT_PROVIDERS)
+### GET list of printify blueprints
+# endpoint = os.path.join(URL_BASE, URL_BLUEPRINTS)
 # response = requests.get(endpoint, headers=HEADER)
-# for provider in response.json():
-# 	if provider['location']['country'] == 'US':
-# 		print(provider)
+# print(f'GET {endpoint}')
+# print(f'response is {response}')
 
+### Filter Out Poster and Canvas Blueprints
+# for blueprint in response.json():
+# 	if any(x in blueprint['title'] for x in ['Canvas', 'Poster']):
+# 		print(f'\n{blueprint}\n')
 
+### ID: 97 - Poster
+### ID: 443 - Posters (EU)
+### ID: 492 - Premium Framed Vertical Poster
+### ID: 493 - Premium Framed Horizontal Poster
+### ID: 50 - Canvas Gallery Wrap
+### ID: 190 - Vertical Framed Premium Gallery Wrap Canvas
+### ID: 193 - Horizontal Framed Premium Gallery Wrap Canvas
+### ID: 196 - Square Framed Premium Gallery Wrap Canvas
+### ID: 534 - Wood Canvas
+poster_blueprint_ids = [97, 443, 492, 493]
+canvas_blueprint_ids = [50, 190, 193, 196, 534]
 
-#### GET list of printify blueprints
-endpoint = os.path.join(URL_BASE, URL_BLUEPRINTS)
-print(f'GET {endpoint}')
-response = requests.get(endpoint, headers=HEADER)
-print(f'response is {response}')
+### GET list of poster printify providers
+for pid in poster_blueprint_ids:
+	endpoint = os.path.join(URL_BASE, 'v1/catalog/blueprints/', str(pid), 'print_providers.json')
+	response = requests.get(endpoint, headers=HEADER)
+	print(f'\nGET {endpoint}')
+	for provider in response.json():
+		print(provider)
 
-for blueprint in response.json():
-	if 'Poster' in blueprint['title']:
-		print(f'\n{blueprint}\n')
-
+### GET list of poster printify providers
+# for pid in poster_blueprint_ids:
+# 	endpoint = os.path.join(URL_BASE, 'v1/catalog/blueprints/', str(pid), 'print_providers.json')
+# 	response = requests.get(endpoint, headers=HEADER)
+# 	print(f'GET {endpoint}')
+# 	print(f'response is {response.content}')
